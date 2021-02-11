8a044fe52d4a7f7658af4f13e77ff7bbb3642f8a
Joel Taroreh <600022140@fjuhsd.org>
2021-02-11 00:55:37 +0000

    complete

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -12,10 +12,38 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+    //declare ArrayList<Integer> factors
+    ArrayList<Integer> factors = new ArrayList<Integer>();
+    //for loop from 2 to number-1 (int i as control variable)
+    for(int i = 2; i < number; i++) {
+      //if statement, if number % i equals 0
+      if(number % i == 0) {
+        //add i to factors
+        factors.add(i);
+      }
+    }
+    //return factors
+    return factors;
+
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+    //initialize int x to 0
+    int x = 0;
+    //while loop, if x is less than the size of nums - 1
+    while(x<nums.size()) {
+      //if statement, if the (size of (getListOfFactors of (element x of nums))) is <= 1
+      if(getListOfFactors(nums.get(x)).size() <= 1) {
+        //remove element x of nums
+        nums.remove(x);
+      }
+      //else
+      else {
+        //add 1 to x
+        x++;
+      }
+    }
+
  }
 }
